e0cbf078692a14104a1d9d399b218ace1b64306b
Jared Peter <aljapeter05@gmail.com>
2026-08-10 23:45:08 -0300

    added result pattern

---
+++ SRM.Api/Utils/ResultExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SRM.Api.Utils
+{
+    public static class ResultExtension
+    {
+        public static ActionResult<T> ToActionResult<T>(this Result<T> result)
+        {
+            if (result.Success)
+                return new OkObjectResult(result.Value);
+
+            return new ObjectResult(new { error = result.Error, field = result.Field }) { StatusCode = result.StatusCode };
+        }
+    }
+}
