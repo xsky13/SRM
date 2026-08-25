638807d73466ad6ca4577ba947ae4a492b784f05
Jared Peter <aljapeter05@gmail.com>
2026-08-25 10:12:22 -0300

    added apartment service interface and dto

---
+++ SRM.Api/Models/Dto/ApartmentDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SRM.Api.Models.Dto
+{
+    public record ApartmentDto(
+        Guid Id,
+        string Name,
+        string Description,
+        float Price,
+        string Location,
+        bool IsDeleted
+    );
+}
---
+++ SRM.Api/Services/IApartmentService.cs
@@ -0,0 +1,11 @@
+﻿
+using SRM.Api.Models.Dto;
+using SRM.Api.Utils;
+
+namespace SRM.Api.Services
+{
+    public interface IApartmentService
+    {
+        Task<Result<ApartmentDto[]>> GetAll(int limit = 10);
+    }
+}
