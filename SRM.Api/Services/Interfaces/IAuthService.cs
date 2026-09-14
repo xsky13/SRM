868ab210a46bea228435ef0be72a8cc24555be20
Jared Peter <aljapeter05@gmail.com>
2026-09-14 09:37:54 -0300

    added auth service

---
+++ SRM.Api/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using SRM.Api.Models.Enums;
+using SRM.Api.Utils;
+
+namespace SRM.Api.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Result<string> CreateToken(int id, string email, UserType userType);
+    }
+}
