e6360c1b1941561f0c6dab3267481d097e0f3207
rosalesAlan <varga.alan5725@gmail.com>
2026-09-14 19:57:33 -0300

    Actualizamos la funcion que obtiene las reservas por id, mapeando la lista de pagos (antes no se hacia)

--- SRM.Api/Services/ReservationService.cs
+++ SRM.Api/Services/ReservationService.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore.Query;
 using SRM.Api.Data;
 using SRM.Api.Models.Dto.Apartment;
 using SRM.Api.Models.Dto.Reservation;
+using SRM.Api.Models.Dto.Payment;
 using SRM.Api.Models.Entities;
 using SRM.Api.Services.Interfaces;
 using SRM.Api.Utils;
@@ -27,12 +28,36 @@ namespace SRM.Api.Services
                     ResrevationId = r.Id,
                     CheckInDate = r.CheckInDate,
                     CheckOutDate = r.CheckOutDate,
-                    ApartmentId = r.ApartmentId
+                    ApartmentId = r.ApartmentId,
+                    State = r.State,
+                    CreatedAt = r.CreatedAt,
+                    UpdatedAt = r.UpdatedAt,
+                    Payments = MapPaymentsToDto(r.Payments),
+
                 })
                 .FirstOrDefaultAsync();
 
             return Result<ReservationDetailDto>.Ok(reservation);
-            
+
+        }
+
+        private List<PaymentDto> MapPaymentsToDto(List<Payment> payments)
+        {
+            // esta funcion mapea la lista de pagos a una lista de PaymentDto
+            if (payments == null || payments.Count == 0)
+                return new List<PaymentDto>();
+
+            return payments.Select(p => new PaymentDto(
+                p.Id,
+                p.Amount,
+                p.IsManual,
+                p.IsSign,
+                p.PaymentDate,
+                p.PaymentStatus,
+                p.ReservationId,
+                p.AppUserId,
+                p.TicketId
+            )).ToList();
         }
 
         public async Task<Result<List<ReservationListingDto>>> GetAllByApartmentId(Guid id)
